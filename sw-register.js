4a1c0327359a03f141ae68cb253089a54dccdf71
Семён Замолоцкий <Semrole@yandex.ru>
2023-05-11 01:30:26 +0700

    fix links, readme

--- sw-register.js
+++ sw-register.js
@@ -1,6 +1,6 @@
 "use strict";
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js').then(
+    navigator.serviceWorker.register('/neuro-editor/service-worker.js').then(
         (reg) => console.log('Service worker registered', reg),
         (err) =>  console.log('Service worker not registered', err)
     );
