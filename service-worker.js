c87d42db4e4e70be5c0fe81eaaab4329a97a4598
Семён Замолоцкий <Semrole@yandex.ru>
2023-07-10 00:27:44 +0700

    delete description

--- service-worker.js
+++ service-worker.js
@@ -9,29 +9,6 @@ self.addEventListener('install', (event) =>
         '/neuro-editor/service-worker.js',
         '/neuro-editor/sw-register.js',
         '/neuro-editor/manifest.webmanifest',
-
-        '/neuro-editor/images/favicon.svg',
-        '/neuro-editor/images/favicon512.png',
-        '/neuro-editor/images/favicon128.png',
-        '/neuro-editor/images/favicon-maskable.png',
-        '/neuro-editor/images/cursor.png',
-
-        'https://cdn.jsdelivr.net/npm/@mediapipe/camera_utils/camera_utils.js',
-        'https://cdn.jsdelivr.net/npm/@mediapipe/control_utils/control_utils.js',
-        'https://cdn.jsdelivr.net/npm/@mediapipe/drawing_utils/drawing_utils.js',
-        'https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/selfie_segmentation.js',
-        'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/face_mesh.js',
-
-        'https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/selfie_segmentation_landscape.tflite',
-        'https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/selfie_segmentation_solution_simd_wasm_bin.js',
-        'https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/selfie_segmentation.binarypb',
-        'https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/selfie_segmentation_solution_simd_wasm_bin.wasm',
-
-        'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/face_mesh_solution_packed_assets_loader.js',
-        'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/face_mesh_solution_simd_wasm_bin.js',
-        'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/face_mesh.binarypb',
-        'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/face_mesh_solution_packed_assets.data',
-        'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/face_mesh_solution_simd_wasm_bin.wasm',
     ])))
 );
 
